17032a034b1657c77ab3aa3adc838d2c6b56abd3
salamanderrex <zhouqingyu.rex@gmail.com>
2014-02-04 14:57:06 +0800

    change
    change

--- src/com/avdpover/android/bean/DC_Current_Calibration.java
+++
@@ -1,8 +0,0 @@
-package com.avdpover.android.bean;
-
-public class DC_Current_Calibration  extends avd_message_base_bean{
-	public DC_Current_Calibration(byte [] bytes)
-	{
-		
-	}
-}
--- src/com/avdpover/android/bean/Flux_weaken.java
+++
@@ -1,7 +0,0 @@
-package com.avdpover.android.bean;
-
-public class Flux_weaken extends avd_message_base_bean {
-	public Flux_weaken(byte[] bytes) {
-
-	}
-}
